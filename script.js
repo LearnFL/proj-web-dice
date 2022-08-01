48a0414500fe260f48c0ba7bb69f23ddf6b863da
DR <86169204+LearnFL@users.noreply.github.com>
2022-07-31 20:26:37 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  var choice1 = Math.floor((Math.random() * 6)+1);
+  var path1 ='./images/dice'+choice1+'.png';
+  var choice2 = Math.floor((Math.random() * 6)+1);
+  var path2 ='./images/dice'+choice2+'.png';
+  document.querySelector(".img1").setAttribute("src", path1);
+  document.querySelector(".img2").setAttribute("src", path2);
+  if (choice1 > choice2) {
+    document.querySelector('h1').innerHTML = "🚩 Player 1 Wins!"
+  }else if (choice2 > choice1) {
+    document.querySelector('h1').innerHTML = "Player 2 Wins! 🚩"
+  }
+  else{
+    document.querySelector('h1').innerHTML = "⚔️ Draw! ⚔️"
+  }
+});
+
+function myReload () {
+  window.location.reload(false);
+  return false;
+}
