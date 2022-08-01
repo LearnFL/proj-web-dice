fc7acc7c96169a214dabfc511ee833f299c0d669
DR <86169204+LearnFL@users.noreply.github.com>
2022-08-01 18:22:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var path1 ='./images/dice'+choice1+'.png';
   var choice2 = Math.floor((Math.random() * 6)+1);
   var path2 ='./images/dice'+choice2+'.png';
-  var dice = new Audio('./audio/dice.mov');
+  var dice = new Audio('./audio/dice.mp3');
   dice.play();
   document.querySelector(".img1").setAttribute("src", path1);
   document.querySelector(".img2").setAttribute("src", path2);
